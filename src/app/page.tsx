67432242d7ba089c84db3ebbc92288d1f3827187
Glitch-guy0 <prajwalm9845@gmail.com>
2024-09-09 16:38:15 +0530

    separator and landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Header from "@/components/Header"
-
-
+import Header from "@/components/Header";
+import Prompt from "@/components/Prompt";
+import Contain from "@/components/ui/Contain";
+import Separator from "@/components/ui/Separator";
 
 export default function rootPage() {
   return (
     <div className="w-full h-full bg-zinc-900 text-white px-4 py-2">
-      <Header />
+      <Contain>
+        <Header />
+        <Prompt />
+        <Separator />
+      </Contain>
+
+
     </div>
-  )
+  );
 }
---
+++ src/components/ui/Separator.tsx
@@ -0,0 +1,3 @@
+export default function Separator() {
+  return <div className=" ring-1 rounded ring-zinc-800 my-8 mx-4"></div>;
+}
