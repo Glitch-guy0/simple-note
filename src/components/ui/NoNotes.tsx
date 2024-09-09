c1d92b2d794b985070295e170f516bf02ae438c4
Glitch-guy0 <prajwalm9845@gmail.com>
2024-09-09 17:48:50 +0530

    replace separator to dialog

---
+++ src/components/ui/NoNotes.tsx
@@ -0,0 +1,7 @@
+export default function NoNotes() {
+  return (
+    <>
+      <p className="text-center text-2xl text-slate-700 mt-8">No notes yet</p>
+    </>
+  );
+}
--- src/components/ui/Separator.tsx
+++
@@ -1,3 +0,0 @@
-export default function Separator() {
-  return <div className=" ring-1 rounded ring-zinc-800 my-8 mx-4"></div>;
-}
