c3146bdf1d9c0558de2c78728eb5c8181aa5bc21
Glitch-guy0 <prajwalm9845@gmail.com>
2024-09-09 10:29:37 +0530

    added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Header from "@/components/Header"
+
+
+
 export default function rootPage() {
-  return (<>
-    this is my rootPage
-  </>)
+  return (
+    <div className="w-full h-full bg-zinc-900 text-white px-4 py-2">
+      <Header />
+    </div>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return (
+        <header className="font-bold text-3xl text-center lg:text-left">Simple Notes</header>
+    )
+}
